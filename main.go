9e09d69f5bf6a76f8687fa982ec5829d76649d55
Wxy <wangxiyu191@gmail.com>
2013-11-16 00:23:42 +0800

    add "/api/"

--- main.go
+++ main.go
@@ -11,19 +11,19 @@ func main() {
 	utils.Log(utils.INF, "oos start at "+utils.Config.Listen)
 
 	maps := map[string]lib.HandlerInterface{
-		"/account/signup":          handler.AccountSignup{},
-		"/account/login":           handler.AccountLogin{},
-		"/account/logout":          handler.AccountLogout{},
-		"/account/session_info":    handler.AccountSessionInfo{},
-		"/user/get_user":           handler.UserGet{},
-		"/post/new":                handler.PostNew{},
-		"/post/get_post":           handler.PostGet{},
-		"/post/reply":              handler.PostReply{},
-		"/post/get_replies":        handler.PostGetReplys{},
-		"/post/markup":             handler.PostMark{},
-		"/post/fetch_by_number":    handler.FetchInit{},
-		"/post/fetch_by_last_post": handler.FetchUpdate{},
-		"/post/fetch_more":         handler.FetchMore{},
+		"/api/account/signup":          handler.AccountSignup{},
+		"/api/account/login":           handler.AccountLogin{},
+		"/api/account/logout":          handler.AccountLogout{},
+		"/api/account/session_info":    handler.AccountSessionInfo{},
+		"/api/user/get_user":           handler.UserGet{},
+		"/api/post/new":                handler.PostNew{},
+		"/api/post/get_post":           handler.PostGet{},
+		"/api/post/reply":              handler.PostReply{},
+		"/api/post/get_replies":        handler.PostGetReplys{},
+		"/api/post/markup":             handler.PostMark{},
+		"/api/post/fetch_by_number":    handler.FetchInit{},
+		"/api/post/fetch_by_last_post": handler.FetchUpdate{},
+		"/api/post/fetch_more":         handler.FetchMore{},
 	}
 
 	err := http.ListenAndServe(utils.Config.Listen, lib.NewHttpServer(maps))
